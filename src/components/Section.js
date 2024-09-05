ec94b6c0e304136a40a13a69434204d2b89468a5
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-05 21:17:55 +0600

    Update Section.js

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,7 +5,16 @@ function Section() {
   return (
     <div>
       <section id="home" className="text-center text-light"> 
-      This is home section 
+      <div className="full-screen">
+       <img src="./img/portfolio.png" alt="Portfolio Image" className="image">
+       <p>Istiyaq khan Razin</p>
+       </div>
+       <div className="articles">
+           <article className="article">
+               <h2>HI I AM RAZIN</h2>
+               <p>HI,my name is <strong>Istiyaq khan Razin</strong>.I came from Bangladesh.l live in Sylhet,Bangladesh.I am 15 years old.I’m a student at 8.My favorite subject is Math.My hobbies are surfing  internet.I would like to be a engineer.</p>
+           </article>
+       </div>
     </section>
    <section id="about" className="text-center text-light">
        This is about section

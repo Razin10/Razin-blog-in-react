9fd818c174e73c3e48ab976b74296285e906a860
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-05 18:36:44 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <div className="bg-dark.bg-gradient">
       <div className="headingME">
         <h1 className="heading"><span>RAZIN</span> blog's</h1>
+      </div>
       </div>
     </div>
   );

4e2168bc14cb7a288d494de0c02492c6de5c6fad
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-05 20:51:18 +0600

    Create Navber.js

---
+++ src/components/Navber.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './nav.css';
+
+function Navber() {
+  return (
+    <div>
+      <nav>
+    <ul>
+      <li><a href="#home">home</a></li>
+      <li><a href="#about">about</a></li>
+      <li><a href="#project">project</a></li>
+      <li><a href="#blog">blog</a></li>
+    </ul>
+  </nav>
+    </div>
+  );
+}
+
+export default Navber;

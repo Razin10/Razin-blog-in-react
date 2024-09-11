e7ae63ffc1202e46fca4a6a55af9d6603f9e4bdb
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-11 22:20:21 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navber from './components/Navber';
 import Section from './components/Section';
 import TaskTracker from './components/TaskTracker';
+import Blog from './components/Blog';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     <Navber />
     <Section />
     <TaskTracker />
+    <Blog />
     
       </div>
     </div>

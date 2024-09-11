5471c6c7fc514d64f0f1483a0b8dd98459d8aa5b
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-11 23:42:08 +0600

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,9 +4,9 @@ import './Blog.css'
 function Blog() {
   const [searchTerm, setSearchTerm] = useState('');
   const [cards, setCards] = useState([
-    { id: 1, title: 'Card 1', content: 'This is the content of card 1.', image: './img/dmitry-bukhantsov-pSbYgGBHgfk-unsplash.jpg' }, // Add image property
-    { id: 2, title: 'Card 2', content: 'This is the content of card 2. Search for this.', image: './img/dmitry-bukhantsov-tdBWtHuZFVc-unsplash.jpg' }, // Add image property
-    { id: 3, title: 'Card 3', content: 'This is the content of card 3.', image: './img/sydney-rae-YI_PrcuD8Ps-unsplash.jpg' }, // Add image property
+    { id: 1, title: 'Card 1', content: 'This is the content of card 1.', image: 'dmitry-bukhantsov-pSbYgGBHgfk-unsplash.jpg' }, // Add image property
+    { id: 2, title: 'Card 2', content: 'This is the content of card 2. Search for this.', image: 'dmitry-bukhantsov-tdBWtHuZFVc-unsplash.jpg' }, // Add image property
+    { id: 3, title: 'Card 3', content: 'This is the content of card 3.', image: 'sydney-rae-YI_PrcuD8Ps-unsplash.jpg' }, // Add image property
   ]);
 
   const handleSearch = (event) => {
@@ -27,7 +27,7 @@ function Blog() {
           card.content.toLowerCase().includes(searchTerm.toLowerCase())
         ).map((card) => (
           <div key={card.id} className="card">
-            <img src={require(`./images/${card.image}`)} alt={card.title} /> {/* Dynamic image path */}
+            <img src={require(`./img/${card.image}`)} alt={card.title} /> {/* Dynamic image path */}
             <h2>{card.title}</h2>
             <p>{card.content}</p>
           </div>

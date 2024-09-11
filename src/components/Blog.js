9dadfdedf3cc977fda64cebaf5fd25cd02408684
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-11 23:39:55 +0600

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,9 +4,9 @@ import './Blog.css'
 function Blog() {
   const [searchTerm, setSearchTerm] = useState('');
   const [cards, setCards] = useState([
-    { id: 1, title: 'Card 1', content: 'This is the content of card 1.', image: 'image1.jpg' }, // Add image property
-    { id: 2, title: 'Card 2', content: 'This is the content of card 2. Search for this.', image: 'image2.jpg' }, // Add image property
-    { id: 3, title: 'Card 3', content: 'This is the content of card 3.', image: 'image3.jpg' }, // Add image property
+    { id: 1, title: 'Card 1', content: 'This is the content of card 1.', image: './img/dmitry-bukhantsov-pSbYgGBHgfk-unsplash.jpg' }, // Add image property
+    { id: 2, title: 'Card 2', content: 'This is the content of card 2. Search for this.', image: './img/dmitry-bukhantsov-tdBWtHuZFVc-unsplash.jpg' }, // Add image property
+    { id: 3, title: 'Card 3', content: 'This is the content of card 3.', image: './img/sydney-rae-YI_PrcuD8Ps-unsplash.jpg' }, // Add image property
   ]);
 
   const handleSearch = (event) => {

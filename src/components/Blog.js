fbace42cdd0af6b985ae1fe3ba3ec1f64c8784ba
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-12 07:22:33 +0600

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -4,9 +4,9 @@ import './Blog.css'
 function Blog() {
   const [searchTerm, setSearchTerm] = useState('');
   const [cards, setCards] = useState([
-    { id: 1, title: 'Card 1', content: 'This is the content of card 1.', image: 'dmitry-bukhantsov-pSbYgGBHgfk-unsplash.jpg' }, // Add image property
-    { id: 2, title: 'Card 2', content: 'This is the content of card 2. Search for this.', image: 'dmitry-bukhantsov-tdBWtHuZFVc-unsplash.jpg' }, // Add image property
-    { id: 3, title: 'Card 3', content: 'This is the content of card 3.', image: 'sydney-rae-YI_PrcuD8Ps-unsplash.jpg' }, // Add image property
+    { id: 1, title: 'Card 1', content: 'This is the content of card 1.', image: 'dmitry-bukhantsov-pSbYgGBHgfk-unsplash.jpg', link:'https://getbootstrap.com/docs/5.0/utilities/text/', linktext:'google' }, // Add image property
+    { id: 2, title: 'Card 2', content: 'This is the content of card 2. Search for this.', image: 'dmitry-bukhantsov-tdBWtHuZFVc-unsplash.jpg', link:'https://getbootstrap.com/docs/5.0/utilities/text/', linktext:'test'}, // Add image property
+    { id: 3, title: 'Card 3', content: 'This is the content of card 3.', image: 'sydney-rae-YI_PrcuD8Ps-unsplash.jpg', link:'https://getbootstrap.com/docs/5.0/utilities/text/', linktext:'razin'}, // Add image property
   ]);
 
   const handleSearch = (event) => {
@@ -30,6 +30,9 @@ function Blog() {
             <img src={require(`./img/${card.image}`)} alt={card.title} /> {/* Dynamic image path */}
             <h2>{card.title}</h2>
             <p>{card.content}</p>
+            <a href={card.link} target="_blank" rel="noopener noreferrer">
+                  {card.linktext}
+                </a>
           </div>
         ))}
       </div>

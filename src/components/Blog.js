bbe974f5541a122417d4a665dabedb8d8cd95a11
Razin10 <167620097+Razin10@users.noreply.github.com>
2024-09-12 21:34:02 +0600

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -30,7 +30,7 @@ function Blog() {
             <img src={require(`./img/${card.image}`)} alt={card.title} /> {/* Dynamic image path */}
             <h2>{card.title}</h2>
             <p>{card.content}</p>
-            <a className="cardlink font-monospace link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover" href={card.link} target="_blank" rel="noopener noreferrer">
+            <a className="cardlink font-monospace btn btn-primary" href={card.link} target="_blank" rel="noopener noreferrer">
                   {card.linktext}
                 </a>
           </div>
